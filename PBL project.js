910f60c183fc5a3be1e801c8644dfae02c73fca3
AdwaitJadhav <103038732+AdwaitJadhav@users.noreply.github.com>
2022-06-11 14:30:37 +0530

    Add files via upload

--- PBL project.js
+++ PBL project.js
@@ -16,27 +16,61 @@ function search(){
 
 }
 
-let d = document.getElementsByClassName('list-img')[0];
-let c = document.getElementsByClassName('book-text')[0];
-let t = document.getElementsByClassName('list')[0];
+let d = document.getElementsByClassName('list-img');
+let c = document.getElementsByClassName('book-text');
+let t = document.getElementsByClassName('list');
 
-function disc(){
-    c.style.display = "block";
-    d.style.display = "none";
-    t.style.backgroundColor = "rgb(0, 132, 255)";
-    t.style.height = "300px";
-    t.style.width = "230px";
-    d.style.height = "280px";
-    d.style.width = "230px";
 
-}
-
-function up(){
-    c.style.display = "none";
-    d.style.display = "block";
-    t.style.backgroundColor = "white";
-    
+function disc(i){
+    c[i].style.display = "block";
+    d[i].style.display = "none";
+    t[i].style.backgroundColor = "rgb(0, 132, 255)";
+    t[i].style.height = "300px";
+    t[i].style.width = "230px";
+    d[i].style.height = "280px";
+    d[i].style.width = "230px";
 
 }
 
+function up(i){
+    c[i].style.display = "none";
+    d[i].style.display = "block";
+    t[i].style.backgroundColor = "white";
+
+}
+
+$(".list").mousedown(function() {
     
+});
+
+$(document).ready(function(){
+    $(".list").mousedown(function(){
+        var a = $(this).index(".list");
+        $(".list").eq(a).css({"background-color":"rgb(0, 132, 255)","height":"300px","width":"230px",});
+        $(".list-img").eq(a).css({"display":"none","height":"280px","width":"230px"});
+        $(".book-text").eq(a).css({"display":"inline-block"});
+    
+        });
+    $(".list").mouseleave(function(){
+        var a = $(this).index(".list");
+        $(".list").eq(a).css({"background-color":"white","height":"30%","width":"auto"});
+        $(".list-img").eq(a).css({"display":"block","height":"250px","width":"200px"});
+        $(".book-text").eq(a).css({"display":"none"});
+        
+        });
+
+    $(".list").mouseenter(function(){
+        var a = $(this).index(".list");
+        $(".list-img").eq(a).css({"display":"block"});
+        $(".list").eq(a).css({"background-color":"white"});
+        $(".book-text").eq(a).css({"display":"none"});
+        $(".list").eq(a).animate({height:'300px',width:'230px'},"fast");
+        $(".list-img").eq(a).animate({height:'280px',width:'230px'},);
+            
+        });
+            
+
+    });
+    
+    
+
